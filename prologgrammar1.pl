c1db1e5dd0cf404e64cb3523c2b1c6696573f7f2
EoinM95 <murphe43@tcd.ie>
2017-02-01 14:42:15 +0000

    Most working except prep distinction

--- prologgrammar1.pl
+++ prologgrammar1.pl
@@ -12,6 +12,7 @@
 
 word(det,[the]).
 word(det,[a]).
+word(det,[an]).
 word(det,["sandy's"]).
 
 word(p,[in]).
@@ -26,12 +27,11 @@ word(a(npred),[former]).
 word(vt,[hate],[np],[np],bse).
 word(vt,[hated],[np],[np],fin).
 word(vt,[like],[np],[np],bse).
-word(vt,[liked],[np],[np],fin).
+word(vt,[liked],[np],[np,vcompclause,s(inf)],fin).
 word(vt,[ask],[np],[np],bse).
-word(vt,[asked],[np],[np],fin).
+word(vt,[asked],[np],[np,whetherclause,whoclause],fin).
 word(vt,[asked],[np],[s(comp),s(inf)],fin).
 word(vt,[looked,up],[np],fin).
-
 
 % word/4 cat, phon, subcat/2, form
 word(vt,[sold],[np],[np],fin).
@@ -39,13 +39,24 @@ word(vt,[looked,up],[np],[np],fin).
 word(vt,[looked],[np],[pp(X)],fin).
 word(vt,[painted],[np],[np],fin).
 word(vt,[painted],[np],[np],ppl). %subj + obj
-word(vt,[knew],[np],[s(fin),s(comp),s(inf)],fin).
+word(vt,[knew],[np],[s(fin),s(comp),s(inf),vcompclause,whetherclause,whoclause],fin).
 word(vt,[knew],[np],[np],fin).
-word(vt,[thought],[np],[s(fin),s(comp),s(nv),s(inf)],fin).
-word(vt,[believed],[np],[s(fin),s(nv),s(inf),vcompclause(fin)],fin).
+word(vt,[thought],[np],[s(fin),s(comp),s(nv),s(inf),vcompclause],fin).
+word(vt,[believed],[np],[s(fin),s(inf),vcompclause,s(nv)],fin).
 word(vt,[believed],[np],[np],fin).
-word(vt,[denied],[np],[s(fin),s(nv),s(inf),vcompclause(fin)],fin)
-word(vt,[suggested],[np],[np,s(fin)],fin).
+word(vt,[denied],[np],[s(fin),s(nv),s(comp),vcompclause],fin).
+word(vt,[doubted],[np],[vcompclause,np,s(fin),s(inf)],fin).
+word(vt,[hated],[np],[vcompclause,s(inf)],fin).
+word(vt,[suggested],[np],[np,s(fin),vcompclause,whetherclause,whoclause],fin).
+word(vt, [persuaded], [np], [np, s(inf)], fin).
+word(vt, [proved], [np], [vcompclause,s(inf),s(fin),s(nv),whetherclause], fin).
+word(vt,[queried],[np],[np,whetherclause,whoclause],fin).
+word(vt,[refuted],[np],[np,vcompclause],fin).
+word(vt,[regretted],[np],[s(fin),vcompclause,s(inf)],fin).
+word(vt,[verified],[np],[s(fin),vcompclause,whetherclause,whoclause],fin).
+word(vt, [wanted], [np], [np, s(inf)], fin).
+word(vt,[wondered],[np],[whetherclause,whoclause],fin).
+word(vt,[said],[np],[s(fin),vcompclause,whetherclause,whoclause],fin).
 word(vd,[gave],[np],[pp(to),np],[np,pp([to])],fin).
 word(vi,[sneezed],[np],fin).
 word(va,[was],[np],[vp],fin).
@@ -58,6 +69,7 @@ word(name,[lee]).
 word(n,[detail]).
 word(n,[details]).
 word(n,[dog]).
+word(n,[ass]).
 word(n,[cat]).
 word(n,[coffee]).
 word(n,[nose]).
@@ -66,7 +78,6 @@ word(n,[picture]).
 word(n,[pharmacology]).
 word(pn,[it]).
 word(relpro,[what]).
-word(relpro,[who]).
 word(relpro,[that]).
 word(comp,[that]).
 word(comp,[whether]).
@@ -131,8 +142,19 @@ relc(Rtype,RS) :-
         word(relpro,Rtype),
         vp(vt,VS,fin).
 
-vcompclause(fin,[that|S]) :-
+vcompclause([that|S]) :-
     s(fin,S).
+
+whetherclause([whether|S]) :-
+  s(fin,S).
+
+whoclause([who|S]) :-
+  %slightly hacky but the only way I could get this to work.
+  %Ideally it would be any pronoun but the result of word(pn,X)
+  %is always X = [it], thus [[it]|S] is not a valid sentence
+  %making this a bit awkward
+  s(fin,[it|S]).
+
 
 p(PS,PS) :-
         word(p,PS).
@@ -255,7 +277,7 @@ p(PS) :-
 
 pp(PS,PP) :-
         append([PS,NS],PP),
-	p(PS),
+	      p(PS),
         np(NS).
 
 % coordinated PPs (must be same sort; syncategorematic).
